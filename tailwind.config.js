954c69ae27cd021f9bed540ce0b77f20ade211be
Sakibur Rahman Sabbir <106433632+gmsakibursabbir@users.noreply.github.com>
2024-10-21 00:12:23 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lexend: ["Lexend", "sans-serif"],
+      },
+      letterSpacing: {
+        "2percent": "2%",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
+        "3xl": "1600px",
+      },
+    },
+  },
+  plugins: [],
+};
